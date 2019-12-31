165f34329376b0fd38737fae2c8884b024e8ea49
Marinosbitter <marijn_bitter@live.nl>
2019-12-31 12:40:55 +0100

    Added basic wordpress plugin setup

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Plugin Name:       Stellaris game mechanics
+ * Plugin URI:        https://code.marinosbitter.nl/plugins/stellaris-game-mechanics/
+ * Description:       Handle the basics with this plugin.
+ * Version:           1.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Marinosbitter
+ * Author URI:        https://marinosbitter.nl/
+ * Text Domain:       stellaris-game-mechanics
+ * Domain Path:       /languages
+ */
+
+//<html>
+//    <head>
+//    </head>
+//    <body>
+//        <script src="/d3/d3.min.js"></script>
+//        <script src="/js/functions.js"></script>
+//        <script>
+//            
+//        </script>
+//    </body>
+//</html>
