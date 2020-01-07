a80b7ff07b8dbda1374f1fa6b20fce8be7371dac
Marinosbitter <marijn_bitter@live.nl>
2020-01-07 12:28:46 +0100

    Some cleanups and testing

--- dist/classes/enqueues.php
+++
@@ -1,8 +0,0 @@
-<?php
-function sgm_enqueue_stylesheets() 
-{
-    wp_enqueue_style( 'index', SGMURL . '/css/index.css' );
-    wp_enqueue_script( 'beforeafter_image', SGMURL . '/js/shortcodes/beforeafter_image.js', array(), false, true);
-}
-
-add_action('wp_enqueue_scripts', 'sgm_enqueue_stylesheets');
